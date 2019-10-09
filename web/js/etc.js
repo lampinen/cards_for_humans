dfcc911afbb00e28df069ee3118167c895b0f871
Andrew Lampinen <lampinen@stanford.edu>
2019-10-09 12:52:59 -0700

    sampling hand

--- web/js/etc.js
+++ web/js/etc.js
@@ -1,3 +1,8 @@
+// chooses a random element from an array 
+function choice(a) {
+    return a[Math.floor(Math.random() * a.length)];
+}
+
 // chooses a random element from an array, return its index as well 
 function argchoice(a) {
     var index = Math.floor(Math.random() * a.length)
